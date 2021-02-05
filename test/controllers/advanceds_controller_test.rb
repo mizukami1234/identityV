15d2fbea7e441ce2051784ae7c81d7ba79e64c8c
mizukami1234 <mizukami19960914@gmail.com>
2021-02-05 12:50:24 +0900

    変更

---
+++ test/controllers/advanceds_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AdvancedsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/beginners_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BeginnersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
