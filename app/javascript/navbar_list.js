0f830f8d43a6177aefde5a85f026c567702eef42
mizukami1234 <mizukami19960914@gmail.com>
2021-02-05 12:47:19 +0900

    headerのjs

---
+++ app/javascript/navbar_list.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function(){
+
+const pullDownButton = document.getElementById("lists")
+const pullDownParents = document.getElementById("pull-down")
+const pullDownChild = document.querySelectorAll(".pull-down-list")
+const currentList = document.getElementById("current-list")
+
+
+pullDownButton.addEventListener('mouseover', function(){
+  pullDownButton.setAttribute("style", "background-color: #FFBEDA;")
+})
+
+pullDownButton.addEventListener('mouseout', function(){
+  pullDownButton.removeAttribute("style", "background-color: #FFBEDA;")
+})
+
+
+pullDownButton.addEventListener('click', function(){
+  if (pullDownParents.getAttribute("style") == "display: block;"){
+    pullDownParents.removeAttribute("style", "display: block;")
+  }else{
+      pullDownParents.setAttribute("style", "display: block;")
+    }
+})
+
+pullDownChild.forEach(function(list) {
+  list.addEventListener('click', function() {
+    value = list.innerHTML
+    currentList.innerHTML = value
+  })
+})
+
+})
