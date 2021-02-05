45172fe57b1e79fc690dde12181dce3d02cfd2ae
mizukami1234 <mizukami19960914@gmail.com>
2021-02-05 12:49:30 +0900

    rooting設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'products#index'
+  resources :products, only: :index
+
+  resources :beginners, only: :index
+  resources :intermediates, only: :index
+  resources :advanceds, only: :index
+  resources :experts, only: :index
 end
